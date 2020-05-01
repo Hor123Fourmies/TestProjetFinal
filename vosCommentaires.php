a9628037644dc04445198222f2bf3c61f98ed717
Hortense <hortensere@aol.com>
2020-05-01 22:37:59 +0200

    pagination ok

--- vosCommentaires.php
+++ vosCommentaires.php
@@ -69,45 +69,138 @@ if($sql = "SELECT * FROM site WHERE id=$idSiteGet"){
 
 
 <?php
-}
 
 // Les commentaires
 
     ?>
+
 <p id="pBtn"><button id="btnPostComment"><a href=<?php echo "comment_post.php?id=$idSiteGet"?>>Poster un commentaire</a></button></p>
 
 
-    <h3>Commentaires</h3>
+<h3>Commentaires</h3>
 
-    <div id="commentaires_detail">
+        <div id="commentaires_detail">
+
+
     <?php
 
-$sql_comment = "SELECT id, texte_comment, pseudo_user, DATE_FORMAT(date, '%d-%m-%Y') as date FROM commentaires WHERE id_site = $idSite ORDER BY id desc";
-$result_comment = $conn->query($sql_comment);
-echo $conn->error;
+    $total_comment = $conn->query("SELECT COUNT(*) FROM `commentaires` WHERE id_site = $idSiteGet");
+    $row = mysqli_fetch_assoc($total_comment);
+    $cpte_comment = $row['COUNT(*)'];
 
-while($row = $result_comment->fetch_assoc()) {
-    $id = $row['id'];
-    echo $id;
-    echo '<br>';
-    $texteComment = $row['texte_comment'];
-    echo utf8_encode($texteComment);
-    echo '<br>';
-    $pseudo = $row['pseudo_user'];
-    echo $pseudo;
-    echo '<br>';
-    $date = $row['date'];
-    echo $date;
-    echo '<br>';
-}
+    switch ($cpte_comment){
+        case $cpte_comment === 0:
+            echo "Il n'y a aucun commentaire.";
+            break;
+        case $cpte_comment === 1:
+            echo "Il y a un commentaire";
+            break;
+        case $cpte_comment > 1:
+            echo "Il y a ". $cpte_comment. " commentaires";
+            break;
+    }
 
-}
-else{
+    $limite = 4;
+    $nbPages = ceil($cpte_comment/$limite);
+
+    if (!isset($_GET['page'])){
+        $page = 1;
+    }
+    else{
+        $page = $_GET['page'];
+    }
+
+    $this_page_first_result = ($page - 1) * $limite;
+
+
+    $sql_comment = "SELECT id, texte_comment, pseudo_user, DATE_FORMAT(date, '%d-%m-%Y') as date FROM commentaires WHERE id_site = $idSiteGet ORDER BY id desc LIMIT $this_page_first_result,$limite";
+    //$result_comment = $conn->query($sql_comment);
+    $result_comment = mysqli_query($conn, $sql_comment);
+    $conn->query($sql_comment);
     echo $conn->error;
-}
+
+    /*
+    while($row = $result_comment->fetch_assoc()) {
+        $id = $row['id'];
+        echo $id;
+        echo '<br>';
+        $texteComment = $row['texte_comment'];
+        echo utf8_encode($texteComment);
+        echo '<br>';
+        $pseudo = $row['pseudo_user'];
+        echo $pseudo;
+        echo '<br>';
+        $date = $row['date'];
+        echo $date;
+        echo '<br>';
+    }
+    */
 
 
+    while ($row = mysqli_fetch_array($result_comment)){
+        $texteComment = $row['texte_comment'];
+        $pseudo = $row['pseudo_user'];
+        $date = $row['date'];
 
-?>
 
+        ?>
+        <p><?php echo utf8_encode($texteComment)?></p>
+        <span><?= "Rédigé par ".$pseudo." le ".$date ?> </span>
+
+        <?php
+    }
+
+    ?>
+
+    <div id="divPagination">
+        <?php
+        for ($page = 1; $page <= $nbPages; $page++){
+            echo '<a href="vosCommentaires.php?id='.$idSiteGet.'&page=' . $page . '">' . $page . '</a> ';
+        }
+
+        ?>
     </div>
+        </div>
+<?php
+
+    }
+    }
+
+    else{
+    echo $conn->error;
+    }
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+    </body>
