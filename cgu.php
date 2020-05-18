70c1d303678ee3abaa138d6a162b59b1ed3ea55b
Hortense <hortensere@aol.com>
2020-05-18 21:26:56 +0200

    finitions footer

--- cgu.php
+++ cgu.php
@@ -9,28 +9,37 @@ include "nav2.php";
     <div style="margin-left: 10%; margin-right: 10%">
         <h4>Description du service</h4>
         "Chimay coups de coeur" est un site internet à un but informatif.
-        Les renseignements fournis sont destinés à faire connaître les sites touristiques de la région de Chimay.
+        Les renseignements qui en font partie sont destinés à faire connaître certains lieux touristiques de la région de Chimay.
+
+        <h4>Définitions</h4>
+        Utilisateur : ce terme désigne toute personne qui utilise le site.
+        <br>
+        Contenu utilisateur : ce sont les données transmises par l'utilisateur au sein du site.
+        <br>
+        Membre : l'utilisateur devient membre lorqu'il est identifié sur le site.
+        Pseudo et mot de passe : ce sont l'ensemble des informations nécessaires à l'identification de l'utilisateur sur le site.
+        L'identifiant et le mot de passe permettent à l'utilisateur d'accéder à des services réservés au membre du site (comme poster des commentaires).
+        Le mot de passe est confidentiel.
 
         <h4>Informations fournies</h4>
-        Bien que ce site ait été préparé avec soin, nous ne pouvons garantir ni l’exhaustivité ni l’exactitude du
-        contenu.
         Les informations affichées sur ce site internet sont extraites d’une base de données dont les renseignements
         sont rédigés par des tiers sous leur seule responsabilité.
+        Nous ne pouvons garantir ni l’exhaustivité ni l’exactitude du
+        contenu.
         Il appartient à l'internaute d'utiliser les informations y figurant avec prudence et de procéder à toute
         vérification sous sa responsabilité exclusive.
 
-        L’administrateur se réserve le droit de corriger, modifier ou supprimer le contenu de ce site à tout moment et
-        sans préavis.
+        L’administrateur se réserve le droit de corriger, modifier ou supprimer le contenu de ce site à tout moment.
 
         D’autre part et conformément à la jurisprudence, les erreurs ou omissions involontaires qui auraient pu
-        subsister, malgré nos soins, ne sauraient engager la responsabilité de "Chimay coups de coeur".
+        subsister malgré nous ne sauraient engager la responsabilité de "Chimay coups de coeur".
 
         <h4>Avis d’internautes</h4>
         "Chimay coups de coeur" intègre des commentaires de membres.
-        Dans ses publications, le membre s'engage à respecter les règles de droit en vigueur.
+        Dans ses publications, le membre s'engage à respecter les règles de droit en vigueur. L'administrateur se réserve un droit de réponse.
 
         <h4>Liens hypertextes</h4>
-        "Chimay coups de coeur" met à la disposition de ses utilisateurs des liens avec d’autres sites internet.
+        "Chimay coups de coeur" met à la disposition de ses utilisateurs des liens vers d’autres sites internet.
         Il ne gère pas et ne contrôle pas le contenu desdits sites et ne pourrait être tenu pour responsable des
         informations y figurant.
 
@@ -38,10 +47,8 @@ include "nav2.php";
         La tentative d’accès ou l’accès à des zones protégées et réservées à l'administrateur sont strictement interdits.
 
         <h4>Durée</h4>
-        Les présentes conditions s’appliquent pendant toute la durée de mise en ligne du site.
+        Les présentes conditions s’appliquent pendant toute la durée de mise en ligne du site et peuvent être modifiées à tout moment.
 
-        <h4>Loi applicable et compétence</h4>
-        Les présentes conditions sont soumises à la réglementation belge et peuvent être modifiées à tout moment.
     </div>
 
 <?php
--- polConf.php
+++ polConf.php
@@ -17,21 +17,9 @@ include "nav2.php";
         <p>
             Les données personnelles collectées sont destinées exclusivement à un usage interne. En aucun cas ces
             données ne seront cédées ou vendues à des tiers.
-            Vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données vous
-            concernant (loi "Informatique et Libertés" du 6 janvier 1978 modifiée).
-            Pour toute demande particulière, contactez-nous par notre formulaire de contact.
-            <a href=contact_form.php></a>
-        </p>
-
-
-        <h4>Publication de contenu</h4>
-        <p>
-            Lorsque vous postez un commentaire sur ce site, celui-ci peut être modéré par l'administrateur du site.
-            En effet, certains types de contenu sont à proscrire sur les sites internet.
-            Il convient alors que chaque message que vous laissez puisser être lu et corrigé (voire supprimé) par
-            l'administrateur du site.
-            Tout ceci permet d'avoir du contenu de qualité et surtout du contenu adéquat vis-à-vis de la législation.
-            Je vous remercie de votre compréhension.
+            Pour toute demande concernant la modification ou la suppression de vos données, veuillez nous écrire via notre formulaire de contact.
+            <br><br>
+            <a href=contact_form.php>Formulaire de contact</a>
         </p>
 
     </div>
